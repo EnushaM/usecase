8217df2e097680d71856a93334d42fd39dcc15af
ENUSHA M <85446900+EnushaM@users.noreply.github.com>
2023-03-06 14:04:25 +0530

    Add files via upload

--- src/Func.js
+++ src/Func.js
@@ -2,24 +2,25 @@ import React from "react";
 import { useState,useEffect } from "react";
 import video from "./Pics/video.mp4";
 
-function Func(){
-    const[a,setInc]=useState(0);
-    const[sec,setSeconds]=useState(0);
+function Func() {
+    const[x,set_x]=useState(0)
     useEffect(()=>{
-        setTimeout(()=>{setInc(a+1);},1000);
-        setTimeout(() => alert('Initial timeout!'),);
-
-        const interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);}, 10);
-          return () => clearInterval(interval);
-        },[])
-    return(
-        <div>
-            <center>
-                <video src={video}/>
-                <h1>{a}</h1>
-            </center>
-        </div>
-    );
+        setTimeout(()=>{
+            if(x==1){alert(" Opening Video ")}
+            else if(x==15){alert("Half a way bending")}
+            else if(x==36){alert("Video Ended"); }
+            set_x(x+1)
+        },1000)
+    })
+  return (
+    <div>
+        <center>
+            <video width="400" height="400" controls>
+                <source src={video}/>
+            </video>           
+            <h1>{x}</h1>
+        </center>
+    </div>
+  )
 }
 export default Func;
